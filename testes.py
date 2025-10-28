d62cee96b884b686fc463b7296d886f3d5e06878
yV1ctorr <yagomelo@yahoo.com>
2025-10-28 14:11:17 -0300

    caminho para home(testes)

--- testes.py
+++ testes.py
@@ -6,43 +6,64 @@ import time
 # Inicializa o navegador
 driver = webdriver.Chrome()
 
-url_inicial = 'file:///C:/Users/Yago Victor/Documents/fork_progressfit/progressfit/login.html'
-driver.get("url_incial")
 
-time.sleep(5)
+url_login = 'file:///C:/Users/Yago Victor/Documents/fork_progressfit/progressfit/login.html'
+driver.get(url_login)
+
+time.sleep(2)
 
 #sendkeys e clear()
 campo_email = driver.find_element(By.ID, "email")
 campo_email.send_keys("teste@progressfit.com")
 
+time.sleep(1)
+
 campo_senha = driver.find_element(By.ID, "password")
 campo_senha.send_keys("123teste")
 
+time.sleep(2)
 
 campo_email.clear()
 campo_senha.clear()
 
 #--------------------------------------------------------
 
-# --- Teste 2: get_attribute() ---
-valor_digitado = campo_usuario.get_attribute("value")
-print("Valor digitado no campo usuário:", valor_digitado)
+campo_email = driver.find_element(By.ID, "email")
+campo_email.send_keys("yago@progressfit.com")
 
+time.sleep(1)
 
-# --- Teste 4: send_keys() + Enter ---
-campo_usuario.send_keys("standard_user")
 campo_senha = driver.find_element(By.ID, "password")
-campo_senha.send_keys("secret_sauce")
-campo_senha.send_keys(Keys.ENTER)
+campo_senha.send_keys("123teste")
 
+time.sleep(1)
+
+button = driver.find_element(By.ID, "btn")
+button.click()
+url_home = 'file:///C:/Users/Yago Victor/Documents/fork_progressfit/progressfit/home.html'
+driver.get(url_home)
 time.sleep(5)
 
-# --- Teste 5: current_url ---
-print("URL atual:", driver.current_url)
 
-# --- Teste adicional: is_displayed() ---
-botao_menu = driver.find_element(By.ID, "react-burger-menu-btn")
-print("Botão de menu visível?", botao_menu.is_displayed())
+# --- Teste 2: get_attribute() ---
+# valor_digitado = campo_email.get_attribute("value")
+# print("Valor digitado no campo usuário:", valor_digitado)
 
-# Fecha o navegador
-driver.quit()
+
+# # --- Teste 4: send_keys() + Enter ---
+# campo_usuario.send_keys("standard_user")
+# campo_senha = driver.find_element(By.ID, "password")
+# campo_senha.send_keys("secret_sauce")
+# campo_senha.send_keys(Keys.ENTER)
+
+# time.sleep(5)
+
+# # --- Teste 5: current_url ---
+# print("URL atual:", driver.current_url)
+
+# # --- Teste adicional: is_displayed() ---
+# botao_menu = driver.find_element(By.ID, "react-burger-menu-btn")
+# print("Botão de menu visível?", botao_menu.is_displayed())
+
+# # Fecha o navegador
+# driver.quit()
